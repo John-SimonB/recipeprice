c3e45f74411fd993ff090ae09208f06dcee6d630
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2023-06-02 16:09:57 +0200

    Code veränderungen im Scraper

--- scraper.py
+++ scraper.py
@@ -4,54 +4,48 @@
 Created on Thu May 11 10:16:45 2023
 
 @author: john-simonbachhuber
-DataScraper für Angebote von Flink, folgende Daten werden ermittelt:
-Produktname, Angebotspreis, Normalpreis, Menge(Verpackung), Produktlink
 """
 
+import uuid
 import requests
 from bs4 import BeautifulSoup
-#Test
+from harperDB import insert_item, delete_all_items, get_all_item, delete_item
 
-url = "https://www.bringmeister.de/angebote?isAvailable"
+url = "https://www.bringmeister.de/k/obst-und-gemuese"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+productlist = []
+items = soup.find_all('div', class_="container svelte-igvw6e")
 
 
-def get_page(url):
-    response = requests.get(url)
-    if not response.ok:
-        print("\nWebsite not found: " + response.status_code)
-    else:
-        soup = BeautifulSoup(response.text, "html.parser")
-        print("\nloading data from = \n" + url)
-    return soup
-
-def parse_soup(soup):
-    productlist = []
-    items = soup.find_all('div', class_="container svelte-98e10g")
-    for item in items:
-        product_name = item.find('a', class_='product-name').text.strip()
-        prices = item.find('div', class_='price-container').text.strip()
-        link = item.find('a', class_='product-name')['href']
-        package = item.find('span', class_='svelte-98e10g').text.strip()
+#delete_all_items()
+for item in items:
+    product_name = item.find('a', class_='product-name').text.strip()
+    prices = item.find('div', class_='price-container').text.strip()
+    link = item.find('a', class_='product-name')['href']
+    #package = item.find('span', class_='svelte-98e10g').text.strip()
         
-        
-        price_splited = prices.split("\n")
+    price_splited = prices.split("\n")
+    
+    if len(price_splited) >= 2:
+        normal_price = price_splited[1]
         special_price = price_splited[0]
-        if len(price_splited) >= 2:
-            normal_price = price_splited[1]
-        else:
-            normal_price = 0
+    else:
+        normal_price = price_splited[0]
+        special_price = 0
+
+    data = {
+    "id": str(uuid.uuid4()),
+    "title": str(product_name),
+    "special_price": str(special_price),
+    "normal_price": str(normal_price),
+    "link": "www.bringmeister.de" + str(link),
+    "package": "10 KG"
+    }
+    insert_item(data)
+    #insert_item(product)
+    #productlist.append(product)
 
 
-        product = {
-            "productname", product_name,
-            "price_special", special_price,
-            "normal_price", normal_price,
-            "package", package,
-            "link", "www.bringmeister.de" + link
-        }
-        productlist.append(product)
-    print(productlist[1])
 
-
-parse_soup(get_page(url))
 
