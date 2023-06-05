1895e519da52427af1fa94bcbcb8567da4a83df6
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2023-06-05 11:15:28 +0200

    chefkoch_recipescraper

--- recpiescrape_chefkoch.py
+++ recpiescrape_chefkoch.py
@@ -1,17 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.chefkoch.de/rezepte/2352371374010722/Cremiger-Nudelauflauf-mit-Tomaten-und-Mozzarella.html"
+url = "https://www.chefkoch.de/rezepte/1229661228043441/Pizzateig.html"
 
 
 def chefkoch_scrape(URL):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     ingredients = soup.find_all('table', class_='ingredients')
+    zutaten_dict = {}
     for ingredient in ingredients:
-        zutaten = [span.text.strip().replace(' ', '').replace(',', ' ') for span in ingredient.find_all('span') if span.text.strip()]
-        #product_name = zutaten[1::2]
-        #package = zutaten[::2]
-    return zutaten
+        data = [span.text.strip().replace(' ', '').replace(',', ' ') for span in ingredient.find_all('span') if span.text.strip()]
+        for i in range(0, len(data), 2):
+            menge = data[i]
+            zutat = data[i + 1]
+
+            # Trenne die Mengenangabe in Wert und Einheit
+            menge_wert, menge_einheit = menge[:-1], menge[-1]  # Letztes Zeichen entfernen
+
+            # Speichere die Zutatendaten im Dictionary
+            zutaten_dict[zutat] = {
+                'menge_wert': menge_wert,
+                'menge_einheit': menge_einheit
+            }
+    return zutaten_dict
 
 #chefkoch_scrape(url)
