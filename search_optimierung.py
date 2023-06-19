b1b3902e2d31dec55dafa4aa37e048cde7ba0fdf
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2023-06-19 14:19:43 +0200

    Wörter die bei der Suche ersetzt werden müssen

---
+++ search_optimierung.py
@@ -0,0 +1,7 @@
+search_words = [["Cocktailtomaten", "Cherrytomaten"], ["Frühlingszwiebel(n)", "Lauchzwiebeln"],
+["Frühlingszwiebel", "Lauchzwiebeln"], ["Feta-Käse", "Feta"], ["Frühlingszwiebeln", "Lauchzwiebeln"]]
+
+
+
+
+
