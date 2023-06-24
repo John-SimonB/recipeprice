b6e4687514d557603028b7ca9eadfa126f791ff8
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2023-06-24 15:29:30 +0200

    neue wörter und+ wörter die nicht gesucht werden

--- search_optimierung.py
+++ search_optimierung.py
@@ -1,7 +1,9 @@
 search_words = [["Cocktailtomaten", "Cherrytomaten"], ["Frühlingszwiebel(n)", "Lauchzwiebeln"], ["frischhefe", "Frische Hefe"],
-["Frühlingszwiebel", "Lauchzwiebeln"], ["Feta-Käse", "Feta"], ["Frühlingszwiebeln", "Lauchzwiebeln"]]
+["Frühlingszwiebel", "Lauchzwiebeln"],["Tomate(n)", "Tomaten"],["Tomate(n) feste", "Tomaten"],["Paprikapulver edelsüßes", "Paprika"], ["Salatcreme (Miracel Whip) Balance", "Salatcreme"],
+ ["Feta-Käse", "Feta"], ["Erbsen TK", "Erbsen"], ["fleischwurstoder4wienerwürstchen kleingeschnitten", "fleischwurst"], 
+ ["Frühlingszwiebeln", "Lauchzwiebeln"], ["Gewürzgurke(n)", "Gewürzgurke"]]
 
-
+skip_words = ["kleingeschnitten", "gewürfelt", "klein", "geschnitten", "oder", "und", "feste"]
 
 
 
