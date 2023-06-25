282f970d5447ece471bb19f0ce103e120f7b047a
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2023-06-25 12:55:13 +0200

    hinzufügen weiterer wörter für besserer suche

--- search_optimierung.py
+++ search_optimierung.py
@@ -1,7 +1,7 @@
 search_words = [["Cocktailtomaten", "Cherrytomaten"], ["Frühlingszwiebel(n)", "Lauchzwiebeln"], ["frischhefe", "Frische Hefe"],
 ["Frühlingszwiebel", "Lauchzwiebeln"],["Tomate(n)", "Tomaten"],["Tomate(n) feste", "Tomaten"],["Paprikapulver edelsüßes", "Paprika"], ["Salatcreme (Miracel Whip) Balance", "Salatcreme"],
  ["Feta-Käse", "Feta"], ["Erbsen TK", "Erbsen"], ["fleischwurstoder4wienerwürstchen kleingeschnitten", "fleischwurst"], 
- ["Frühlingszwiebeln", "Lauchzwiebeln"], ["Gewürzgurke(n)", "Gewürzgurke"]]
+ ["Frühlingszwiebeln", "Lauchzwiebeln"],["paprikapulver", "paprika"], ["Gewürzgurke(n)", "Gewürzgurke"]]
 
 skip_words = ["kleingeschnitten", "gewürfelt", "klein", "geschnitten", "oder", "und", "feste"]
 
